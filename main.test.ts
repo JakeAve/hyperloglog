16e5ef13c73c52218042e9173142a2a43f6bf13b
JakeAve <jacobjavery@gmail.com>
2024-04-18 00:03:56 -0400

    switched to class

--- main.test.ts
+++ main.test.ts
@@ -1,84 +1,128 @@
-import { assertEquals } from "https://deno.land/std@0.220.0/assert/mod.ts";
-import { countTrailingZeros, hash, sortBucket } from "./main.ts";
+import {
+  assertEquals,
+  assertGreater,
+  assertLess,
+} from "https://deno.land/std@0.220.0/assert/mod.ts";
+import { HyperLogLog, sliceSHA256 } from "./main.ts";
 
-Deno.test("hash() foo works", async () => {
+Deno.test("sliceSHA256() foo works", async () => {
   const byteString = "2c 26 b4 6b 68 ff c6 8f";
   const byteArray = byteString.split(" ").map((b) => parseInt(b, 16));
   const uint8Array = new Uint8Array(byteArray);
 
   const input = "foo";
-  const hashed = await hash(input);
+  const hashed = await sliceSHA256(input);
 
   assertEquals(hashed.length, 8);
   assertEquals(hashed, uint8Array);
 });
 
-Deno.test("countTrailingZeros() 'foo' works", () => {
+Deno.test("getTrailingZeroBits() 'foo' works", () => {
   const byteString = "2c 26 b4 6b 68 ff c6 8f";
   const byteArray = byteString.split(" ").map((b) => parseInt(b, 16));
   const uint8Array = new Uint8Array(byteArray);
 
-  const count = countTrailingZeros(uint8Array);
+  const count = HyperLogLog.getTrailingZeroBits(uint8Array);
   assertEquals(count, 0);
 });
 
-Deno.test("countTrailingZeros() '000' works", () => {
+Deno.test("getTrailingZeroBits() '000' works", () => {
   const arr = new Uint8Array([0, 0, 0]);
-  const count = countTrailingZeros(arr);
+  const count = HyperLogLog.getTrailingZeroBits(arr);
   assertEquals(count, 24);
 });
 
-Deno.test("countTrailingZeros() 8 works", () => {
+Deno.test("getTrailingZeroBits() 8 works", () => {
   const arr = new Uint8Array([12, 10, 8]);
-  const count = countTrailingZeros(arr);
+  const count = HyperLogLog.getTrailingZeroBits(arr);
   assertEquals(count, 3);
 });
 
-Deno.test("countTrailingZeros() 16 works", () => {
+Deno.test("getTrailingZeroBits() 16 works", () => {
   const arr = new Uint8Array([12, 10, 16]);
-  const count = countTrailingZeros(arr);
+  const count = HyperLogLog.getTrailingZeroBits(arr);
   assertEquals(count, 4);
 });
 
-Deno.test("countTrailingZeros() count is added from prev", () => {
+Deno.test("getTrailingZeroBits() count is added from prev", () => {
   const arr = new Uint8Array([12, 8, 0]);
-  const count = countTrailingZeros(arr);
+  const count = HyperLogLog.getTrailingZeroBits(arr);
   assertEquals(count, 11);
 });
 
-Deno.test("sortBucket() first 4 bits of 12", () => {
+Deno.test("getBucket() first 4 bits of 12", () => {
   const arr = new Uint8Array([12, 8, 0]);
-  const foo = sortBucket(arr, 4);
+  const foo = HyperLogLog.getBucket(arr, 4);
   assertEquals(foo, 0);
 });
 
-Deno.test("sortBucket() first 6 bits of 12", () => {
+Deno.test("getBucket() first 5 bits of 12", () => {
   const arr = new Uint8Array([12, 8, 0]);
-  const foo = sortBucket(arr, 6);
+  const foo = HyperLogLog.getBucket(arr, 5);
+  assertEquals(foo, 1);
+});
+
+Deno.test("getBucket() first 6 bits of 12", () => {
+  const arr = new Uint8Array([12, 8, 0]);
+  const foo = HyperLogLog.getBucket(arr, 6);
   assertEquals(foo, 3);
 });
 
-Deno.test("sortBucket() first 8 bits of 12", () => {
-  const arr = new Uint8Array([12, 8, 0]);
-  const foo = sortBucket(arr, 8);
-  assertEquals(foo, 12);
+Deno.test("numBucketsToBits() 16", () => {
+  const numOfBits = HyperLogLog.numBucketsToBits(16);
+  assertEquals(numOfBits, 4);
 });
 
-// Deno.test("hyperLogLog()", async () => {
-//   const options = "abcdefghijklmnopqrstuvwxyz".split("");
-//   const dataSet1 = [];
-//   for (let i = 0; i < 1000000; i++) {
-//     dataSet1.push(options[Math.floor(Math.random() * options.length)]);
-//   }
-//   const foo2 = await hyperLogLog(dataSet1);
-//   console.log(foo2);
-// });
+Deno.test("numBucketsToBits() 32", () => {
+  const numOfBits = HyperLogLog.numBucketsToBits(32);
+  assertEquals(numOfBits, 5);
+});
 
-// Deno.test("hyperLogLog2()", async () => {
-//   const dataSet1 = [];
-//   for (let i = 0; i < 1000000; i++) {
-//     dataSet1.push(Math.random().toString());
-//   }
-//   const foo1 = await hyperLogLog(dataSet1);
-//   console.log(foo1 / 1000000);
-// });
+Deno.test("numBucketsToBits() 64", () => {
+  const numOfBits = HyperLogLog.numBucketsToBits(64);
+  assertEquals(numOfBits, 6);
+});
+
+Deno.test("getAlphaMM() 16", () => {
+  const alphaMM = HyperLogLog.getAlphaMM(16);
+  assertEquals(alphaMM, 0.673);
+});
+
+Deno.test("getAlphaMM() 32", () => {
+  const alphaMM = HyperLogLog.getAlphaMM(32);
+  assertEquals(alphaMM, 0.697);
+});
+
+Deno.test("getAlphaMM() 64", () => {
+  const alphaMM = HyperLogLog.getAlphaMM(64);
+  assertEquals(alphaMM, 0.709);
+});
+
+Deno.test("estimate() alphabet", async () => {
+  const options = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split(
+    "",
+  );
+  const hll = new HyperLogLog(16, sliceSHA256);
+  const proms = [];
+  for (let i = 0; i < 1000000; i++) {
+    proms.push(hll.add(options[Math.floor(Math.random() * options.length)]));
+  }
+  await Promise.all(proms);
+  assertEquals(hll.buckets.length, 16);
+
+  const estimate = hll.estimate();
+  assertLess(estimate, 1);
+});
+
+Deno.test("estimate() numbers", async () => {
+  const hll = new HyperLogLog(16, sliceSHA256);
+  const proms = [];
+  for (let i = 0; i < 1000000; i++) {
+    proms.push(hll.add(Math.random().toString()));
+  }
+  await Promise.all(proms);
+  assertEquals(hll.buckets.length, 16);
+
+  const estimate = hll.estimate();
+  assertGreater(estimate, 1000);
+});
--- main.ts
+++ main.ts
@@ -1,76 +1,107 @@
-export async function hash(input: string, bytes = 8) {
+export async function sliceSHA256(input: string, bits = 64) {
   const encoder = new TextEncoder();
   const data = encoder.encode(input);
   const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-  const sizedBuffer = hashBuffer.slice(0, bytes);
+  const sizedBuffer = hashBuffer.slice(0, bits / 8);
   return new Uint8Array(sizedBuffer);
 }
 
-export function countTrailingZeros(uint8Array: Uint8Array) {
-  let count = 0;
-  for (let i = uint8Array.length - 1; i >= 0; i--) {
-    if (uint8Array[i] === 0) {
-      count += 8;
-    } else {
-      let val = uint8Array[i];
-      while ((val & 1) === 0) {
-        count++;
-        val >>= 1;
-      }
-      break;
+type HashBitOptions = undefined | 32 | 64;
+
+type NumOfBucketsArg = undefined | 16 | 32 | 64;
+
+type NumOfBuckets = 16 | 32 | 64;
+
+type BitsPerBucket = 4 | 5 | 6;
+
+export class HyperLogLog {
+  static readonly ALPHA_VALUES = {
+    16: 0.673,
+    32: 0.697,
+    64: 0.709,
+  };
+  #bitsPerBucket: BitsPerBucket;
+  buckets: Uint8Array;
+  #hashFunction: (value: string, bits: HashBitOptions) => Promise<Uint8Array>;
+
+  constructor(
+    numBuckets: NumOfBucketsArg = 16,
+    hashFunction: (value: string, bits: HashBitOptions) => Promise<Uint8Array>,
+  ) {
+    this.buckets = new Uint8Array(numBuckets).fill(0);
+    this.#hashFunction = hashFunction;
+    this.#bitsPerBucket = HyperLogLog.numBucketsToBits(numBuckets);
+  }
+
+  static numBucketsToBits(numberOfBuckets: 16 | 32 | 64) {
+    let bitsPerBucket = 0;
+    let val = numberOfBuckets;
+    while (val > 1) {
+      val >>= 1;
+      bitsPerBucket++;
     }
-  }
-  return count;
-}
-
-export function sortBucket(uint8arr: Uint8Array, bits = 4) {
-  const maxBits = uint8arr.length * 8;
-  if (bits > maxBits) {
-    throw new Error(
-      `Cannot extract ${bits} bits from an array with only ${maxBits} bits.`,
-    );
+    return bitsPerBucket as BitsPerBucket;
   }
 
-  let result = 0;
-  let bitIndex = 0;
+  static getBucket(uint8arr: Uint8Array, bits: BitsPerBucket) {
+    const maxBits = 8;
+    if (bits > maxBits) {
+      throw new Error(
+        `Cannot extract ${bits} bits from an array with only ${maxBits} bits.`,
+      );
+    }
 
-  for (const byte of uint8arr) {
+    let result = 0;
+    let bitIndex = 0;
+
     for (let i = 7; i >= 0; i--) {
-      const bit = (byte >> i) & 1;
+      const bit = (uint8arr[0] >> i) & 1;
       result = (result << 1) | bit;
       bitIndex++;
       if (bitIndex === bits) {
         return result;
       }
     }
+
+    return result;
   }
 
-  return result;
+  static getTrailingZeroBits(uint8Array: Uint8Array) {
+    let count = 0;
+    for (let i = uint8Array.length - 1; i >= 0; i--) {
+      if (uint8Array[i] === 0) {
+        count += 8;
+      } else {
+        let val = uint8Array[i];
+        while ((val & 1) === 0) {
+          count++;
+          val >>= 1;
+        }
+        break;
+      }
+    }
+    return count;
+  }
+
+  static getAlphaMM(m: NumOfBuckets) {
+    if (m < 128) {
+      return HyperLogLog.ALPHA_VALUES[m] || 0.7213 / (1 + 1.079 / m); // Step 1 and 2: Define αm based on m
+    }
+    return 0.7213 / (1 + 1.079 / m); // Step 2: Use this formula for m ≥ 128
+  }
+
+  async add(input: string) {
+    const hash = await this.#hashFunction(input, 64);
+    const count = HyperLogLog.getTrailingZeroBits(hash);
+    const bucket = HyperLogLog.getBucket(hash, this.#bitsPerBucket);
+    this.buckets[bucket] = Math.max(this.buckets[bucket], count);
+  }
+
+  estimate() {
+    const sum = this.buckets.reduce((acc, rank) => acc + 1 / (1 << rank), 0);
+    const alphaMM = HyperLogLog.getAlphaMM(this.buckets.length as NumOfBuckets);
+    const estimate = alphaMM / sum;
+    // todo: apply bias correction
+    return estimate;
+  }
 }
-
-// export async function hyperLogLog(dataSet: string[], bitsPerBucket = 4) {
-//   const buckets: number[] = [];
-//   const hashes = dataSet.map((d) =>
-//     hash(d).then((a) => {
-//       const count = countTrailingZeros(a);
-//       const bucket = sortBucket(a, bitsPerBucket);
-//       if (!buckets[bucket] || count > buckets[bucket]) {
-//         buckets[bucket] = count;
-//       }
-//     })
-//   );
-//   await Promise.all(hashes);
-//   let sumOfInverses = 0;
-//   let bucketsLength = 0;
-
-//   for (const max of buckets) {
-//     if (max >= 0) {
-//       bucketsLength += 1;
-//       const estimate = Math.pow(2, max);
-//       sumOfInverses += 1 / estimate;
-//     }
-//   }
-//   const mean = bucketsLength / sumOfInverses;
-//   // todo: biasCorrectionFactor
-//   return mean;
-// }
